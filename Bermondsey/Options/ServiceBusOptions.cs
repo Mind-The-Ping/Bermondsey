2490e41453383c09a2eb677a1ff67ca9dd27f98d
Darren <darren.daley@hotmail.co.uk>
2025-09-09 14:23:03 +0100

    made a change to the service bus options file

--- Bermondsey/Options/ServiceBusOptions.cs
+++ Bermondsey/Options/ServiceBusOptions.cs
@@ -1,25 +1,10 @@
 ﻿namespace Bermondsey.Options;
 public class ServiceBusOptions
 {
-    public required string ConnectionString { get; set; }
     public required QueueOptions Queues { get; set; }
-    public required TopicOptions Topics { get; set; }
 }
-
 
 public class QueueOptions
 {
-    public required string Disruptions { get; set; }
     public required string Notifications { get; set; }
-}
-
-public class TopicOptions
-{
-    public required TopicSubscription DisruptionEnds { get; set; }
-}
-
-public class TopicSubscription
-{
-    public required string Name { get; set; }
-    public required string Subscription { get; set; }
 }
