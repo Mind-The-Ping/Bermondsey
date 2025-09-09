896ebd5c4019dd20c66b7bc12eb9f6b36ce608bc
Darren <darren.daley@hotmail.co.uk>
2025-09-09 07:43:20 +0100

    fixed broken tests

--- Bermondsey/DisruptionNotifier.cs
+++ Bermondsey/DisruptionNotifier.cs
@@ -41,67 +41,64 @@ public class DisruptionNotifier
     public async Task<Result> NotifyDisruptionAsync(Disruption disruption)
     {
         var notifiedUsers = await _userNotifiedRepository
-           .GetUsersByDisruptionIdAsync(disruption.Id);
+        .GetUsersByDisruptionIdAsync(disruption.Id);
 
         var localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, _londonTimeZone);
 
-        var affectedUsers = await _waterlooClient
-            .GetAffectedUsersAsync(disruption.Line.Id,
-                                   disruption.StartStationId,
-                                   disruption.EndStationId,
-                                   disruption.Severity,
-                                   TimeOnly.FromDateTime(DateTime.UtcNow),
-                                   localTime.DayOfWeek);
+        var affectedUsers = await _waterlooClient.GetAffectedUsersAsync(
+           disruption.Line.Id,
+           disruption.StartStationId,
+           disruption.EndStationId,
+           disruption.Severity,
+           TimeOnly.FromDateTime(DateTime.UtcNow),
+           localTime.DayOfWeek);
 
-        if(affectedUsers.IsFailure) {
+        if (affectedUsers.IsFailure) {
             return Result.Failure($"Failed to get affected users : {affectedUsers.Error}");
         }
 
-        var ids = affectedUsers.Value.Select(x => x.Id);
-
-        var userDetails = await _stratfordClient
-            .GetUserDetailsAsync(ids);
+        var userDetails = await _stratfordClient.GetUserDetailsAsync(
+            affectedUsers.Value.Select(x => x.Id));
 
         if (userDetails.IsFailure) {
             return Result.Failure($"Failed to get users details : {userDetails.Error}");
         }
 
+        var phoneLookup = userDetails.Value?
+        .ToDictionary(u => u.Id, u => u.PhoneNumber)
+        ?? new Dictionary<Guid, string>();
+
         var usersToNotify = new Dictionary<Guid, User>();
-
-        foreach (var notifiedUser in notifiedUsers)
-        {
-            if(notifiedUser.Severity == disruption.Severity) {
-                continue;
-            }
-
-            usersToNotify[notifiedUser.Id] = notifiedUser;
-        }
-
         var errors = new List<string>();
 
-        var phoneLookup = userDetails.Value?
-            .ToDictionary(u => u.Id, u => u.PhoneNumber)
-            ?? new Dictionary<Guid, string>();
-
-        foreach (var userToNotify in usersToNotify.Values)
+        foreach (var affectedUser in affectedUsers.Value)
         {
-            if (!phoneLookup.TryGetValue(userToNotify.Id, out var phoneNumber))
+            if (!phoneLookup.TryGetValue(affectedUser.Id, out var phoneNumber))
             {
-                errors.Add($"Failed to find phone number for {userToNotify.Id}");
+                errors.Add($"Failed to find phone number for {affectedUser.Id}");
                 continue;
             }
 
             var user = new User(
-                userToNotify.Id,
+                affectedUser.Id,
                 disruption.Id,
                 disruption.Line,
-                userToNotify.StartStation,
-                userToNotify.EndStation,
+                affectedUser.StartStation,
+                affectedUser.EndStation,
                 disruption.Severity,
                 phoneNumber,
-                userToNotify.EndTime);
+                affectedUser.EndTime);
 
             usersToNotify[user.Id] = user;
+        }
+
+        foreach (var oldUser in notifiedUsers)
+        {
+            if (!usersToNotify.ContainsKey(oldUser.Id) &&
+                 oldUser.Severity != disruption.Severity)
+            {
+                usersToNotify[oldUser.Id] = oldUser;
+            }
         }
 
         var finalUsersToNotify = usersToNotify.Values.ToList();
@@ -109,16 +106,16 @@ public class DisruptionNotifier
         foreach (var user in finalUsersToNotify)
         {
             var message = _messageFormatter.FormatDisruption(
-               user.Line.Name,
-               user.StartStation.Name,
-               user.EndStation.Name,
-               user.Severity);
+                user.Line.Name,
+                user.StartStation.Name,
+                user.EndStation.Name,
+                user.Severity);
 
             var messageResult = await _smsClient.SendAsync(user.PhoneNumber, message);
+
             if (messageResult.IsFailure)
             {
-                errors.Add($"Failed to send notification to {user.PhoneNumber} " +
-                    $": {messageResult.Error}.");
+                errors.Add($"Failed to send notification to {user.PhoneNumber}: {messageResult.Error}.");
 
                 await PublishNotificationMessageAsync(
                     Guid.NewGuid(),
@@ -134,21 +131,21 @@ public class DisruptionNotifier
             }
 
             await PublishNotificationMessageAsync(
-                    Guid.NewGuid(),
-                    user.Id,
-                    user.Line.Id,
-                    user.DisruptionId,
-                    user.StartStation.Id,
-                    user.EndStation.Id,
-                    disruption.SeverityId,
-                    NotificationSentBy.Sms);
+                Guid.NewGuid(),
+                user.Id,
+                user.Line.Id,
+                user.DisruptionId,
+                user.StartStation.Id,
+                user.EndStation.Id,
+                disruption.SeverityId,
+                NotificationSentBy.Sms);
         }
 
         await _userNotifiedRepository.SaveUsersAsync(finalUsersToNotify);
 
         return errors.Count != 0
-           ? Result.Failure(string.Join("; ", errors))
-           : Result.Success();
+            ? Result.Failure(string.Join("; ", errors))
+            : Result.Success();
     }
 
     public async Task<Result> NotifyDisruptionResolvedAsync(DisruptionEnd disruptionEnd)
